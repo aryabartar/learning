33955cf0c124efc5f4a6b32143eb7c487961acb2
Arya Khaligh <bartararya@gmail.com>
2019-04-25 09:12:25 +0430

    Create takes table

--- mysql/createTables.sql
+++ mysql/createTables.sql
@@ -1,20 +1,17 @@
-USE university;
-
-
 create table classroom
 	(building		varchar(15),
 	 room_number		varchar(7),
 	 capacity		numeric(4,0),
 	 primary key (building, room_number)
 	);
-SHOW 
 
-create table department
-	(dept_name		varchar(20), 
-	 building		varchar(15), 
-	 budget		        numeric(12,2) check (budget > 0),
-	 primary key (dept_name)
-	);
+
+-- create table department
+-- 	(dept_name		varchar(20), 
+-- 	 building		varchar(15), 
+-- 	 budget		        numeric(12,2) check (budget > 0),
+-- 	 primary key (dept_name)
+-- 	);
 
 create table course
 	(course_id		varchar(8), 
--- mysql/query.sql
+++ mysql/query.sql
@@ -1,1 +1,13 @@
-CREATE TABLE test;
+CREATE TABLE takes(
+    ID VARCHAR(5),
+    course_id VARCHAR(8),
+    sec_id VARCHAR(8),
+    semester VARCHAR(6), 
+    year NUMERIC(4,0),
+    grade VARCHAR(2), 
+    PRIMARY KEY (ID, course_id, sec_id, semester, year),
+    FOREIGN KEY (ID) REFERENCES student(ID),
+    FOREIGN KEY (course_id, sec_id, semester, year) REFERENCES student(course_id, sec_id, semester, year)
+
+)
+;
