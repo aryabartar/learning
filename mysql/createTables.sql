51613597cc840a87483632e1095ee39a7711617d
Arya Khaligh <bartararya@gmail.com>
2019-04-26 20:33:51 +0430

    Add DDL structure

--- mysql/createTables.sql
+++ mysql/createTables.sql
@@ -6,12 +6,12 @@ create table classroom
 	);
 
 
--- create table department
--- 	(dept_name		varchar(20), 
--- 	 building		varchar(15), 
--- 	 budget		        numeric(12,2) check (budget > 0),
--- 	 primary key (dept_name)
--- 	);
+create table department
+	(dept_name		varchar(20), 
+	 building		varchar(15), 
+	 budget		        numeric(12,2) check (budget > 0),
+	 primary key (dept_name)
+	);
 
 create table course
 	(course_id		varchar(8), 
