87d0f805148d1f5ee3dd3f5bf6f6910d2fd9d36e
Arya Khaligh <bartararya@gmail.com>
2019-04-29 22:31:29 +0430

    Great SQL function syntax

--- mysql/query.sql
+++ mysql/query.sql
@@ -1,11 +1,41 @@
-SELECT *
-FROM (
-    SELECT user.user_id, sum(purchase.quantity) AS purchaseQuantity
-    FROM ((user NATURAL JOIN purchase) JOIN product 
-        ON purchase.product_id=product.product_id) JOIN category
-            ON product.category_id=category.category_id
-    WHERE category.name="Sport"
-    GROUP BY user.user_id
-) AS sportBuy
-WHERE sportBuy.purchaseQuantity>3
-;
+-- DELIMITER //
+-- CREATE FUNCTION dept_count2 (dept_name VARCHAR(20)) 
+-- RETURNS INT 
+-- BEGIN
+--     DECLARE temp INT;
+--     SELECT * 
+--     FROM instructor
+--     RETURN 12;
+-- END //
+-- DELIMITER ;
+
+
+DELIMITER //
+CREATE FUNCTION func4 (dept_name VARCHAR(20), ID INT) RETURNS INT
+  BEGIN
+  	DECLARE tt INT;	
+    DECLARE tt1 INT;
+    SELECT count(*) INTO tt1
+    FROM department
+    WHERE department.dept_name=dept_name; 
+    SELECT count(*) INTO tt
+    FROM department
+    WHERE department.dept_name=dept_name;
+    RETURN tt;
+  END //
+DELIMITER ;
+
+
+-- CREATE FUNCTION F_TEST(PID INT) RETURNS VARCHAR
+-- BEGIN
+-- /*DECLARE VALUES YOU MAY NEED, EXAMPLE:
+--   DECLARE NOM_VAR1 DATATYPE [DEFAULT] VALUE;
+--   */
+--   DECLARE NAME_FOUND VARCHAR DEFAULT "";
+
+--     SELECT name INTO NAME_FOUND FROM instructor WHERE ID = PID;
+--   RETURN NAME_FOUND;
+-- END;
+
+-- CREATE FUNCTION hello(id INT) RETURNS VARCHAR(20)
+-- RETURN 'FOO';
