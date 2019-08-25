c1cb81b89682ac8777b79c3cda568cc1fccebab2
Arya <bartararya@gmail.com>
2019-08-25 18:28:52 +0430

    Fix binary_search

--- interview/search/binary_search.py
+++ interview/search/binary_search.py
@@ -1,18 +1,21 @@
 def binary_search(element, array):
     def search(element, array, i, j):
+        middle = int((i + j) / 2)
+
+        if j < i:
+            return False
+
         if i == j:
-            if array[i] == element:
+            if array[middle] == element:
                 return True
             return False
 
-        middle = int((i + j) / 2)
-
-        return (
-                search(element, array, i, middle) or
-                search(element, array, middle + 1, j)
-        )
+        if element > array[middle]:
+            return search(element, array, middle + 1, j)
+        else:
+            return search(element, array, i, middle - 1)
 
     return search(element, array, 0, len(array) - 1)
 
 
-print(binary_search(4, [1, 2, 3, 10, 22, 3, 44, 5, 32, 3]))
+print(binary_search(4, [1, 2, 3, 5, 6, 7, 8, 9, 10]))
